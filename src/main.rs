9839b0288953899e0eef61a632010ca4bc4db02b
chriscargill <chriscargill07@gmail.com>
2021-02-10 22:39:43 -0500

    Added another function
    Tested borrowing values and ownership of variables. As well as utilizing
    and modifying complicated data structures (structs and vectors).
    
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
+    let mut data: Vec<User> = Vec::new();
     println!("Hello, world!");
     println!("{}", add(10, 12));
-    println!("{}", greatest_common_denominator(300,60));
+    println!("{}", greatest_common_denominator(1337,7));
+    data = mutable_data(data); // pass ownership back to the main function by storing it in the variable data
+    data[1].username = String::from("Chris");
+    data[1].active = true;
+    println!("{}_{}:{}/{}", data[1].username, data[1].email, data[1].hashed_pass, data[1].active);
 }
 
 fn add(a: u64, b: u64) -> u64 {
@@ -9,7 +14,7 @@ fn add(a: u64, b: u64) -> u64 {
     c
 }
 
-fn greatest_common_denomenator(mut first_num: u64, mut second_num: u64) -> u64 {
+fn greatest_common_denominator(mut first_num: u64, mut second_num: u64) -> u64 {
     while first_num != 0 {
         if first_num < second_num {  // if the first number is not the largest, switch the two numbers
             let temp_number: u64 = first_num;
@@ -20,4 +25,29 @@ fn greatest_common_denomenator(mut first_num: u64, mut second_num: u64) -> u64 {
     }
     second_num
 
+}
+
+
+struct User {
+    username: String,
+    email: String,
+    hashed_pass: u128,
+    active: bool,
+}
+
+fn mutable_data(mut data: Vec<User>) -> Vec<User> {
+
+    for mut every_number in 0..10 {
+        if every_number % 16 == 0 || every_number % 50 == 0{ // set some values to be zeros
+            every_number = 0;
+        }
+        let user = User{ // add the user struct to data vector
+            username: String::from("none"), 
+            email: String::from("default@home.com"), 
+            hashed_pass: every_number, 
+            active: false};
+        println!("{}_{}:{}/{}", user.username, user.email, user.hashed_pass, user.active);
+        data.push(user);
+    }
+    return data;
 }
