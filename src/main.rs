e5e1a1a306e635b6c8916fb12af37362b50368cb
chriscargill <chriscargill07@gmail.com>
2021-02-10 21:45:15 -0500

    Added function, greatest_common_denominator
    Testing more functions for practice
    
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
     println!("Hello, world!");
-    println!("{}", test(10, 12));
+    println!("{}", add(10, 12));
+    println!("{}", greatest_common_denominator(300,60));
 }
 
-fn test(a: u64, b: u64) -> u64 {
+fn add(a: u64, b: u64) -> u64 {
     let c: u64 = a + b;
     c
 }
 
+fn greatest_common_denomenator(mut first_num: u64, mut second_num: u64) -> u64 {
+    while first_num != 0 {
+        if first_num < second_num {  // if the first number is not the largest, switch the two numbers
+            let temp_number: u64 = first_num;
+            first_num = second_num;
+            second_num = temp_number;
+        }
+        first_num = first_num % second_num
+    }
+    second_num
+
+}
